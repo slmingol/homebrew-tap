33c71ce270d4827a56a69278050cedd71a51303d
goreleaserbot <goreleaser@lamolabs.org>
2021-05-10 03:14:57 -0400

    Brew formula update for gorelease_ex version 0.0.61

--- Formula/gorelease_ex.rb
+++ Formula/gorelease_ex.rb
@@ -5,17 +5,17 @@
 class GoreleaseEx < Formula
   desc "Software to create fast and easy drum rolls."
   homepage "https://www.github.com/slmingol/gorlease_ex"
-  version "0.0.60"
+  version "0.0.61"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/slmingol/gorelease_ex/releases/download/0.0.60/gorelease_ex_0.0.60_Darwin_x86_64.tar.gz"
-    sha256 "a75c1f6bc1c68ffc0ed0aa2bbdba59a2fb8392020c1078a870e982aa11516250"
+    url "https://github.com/slmingol/gorelease_ex/releases/download/0.0.61/gorelease_ex_0.0.61_Darwin_x86_64.tar.gz"
+    sha256 "68b7ad6fc813144292569839680af0a30768e77abda1c10c4cbed3e664a2ee8c"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/slmingol/gorelease_ex/releases/download/0.0.60/gorelease_ex_0.0.60_Linux_x86_64.tar.gz"
-    sha256 "26f2705c44dd78529552fce10f813d24e41d06cd1dd13a9d6ee3a91fc61c3ff0"
+    url "https://github.com/slmingol/gorelease_ex/releases/download/0.0.61/gorelease_ex_0.0.61_Linux_x86_64.tar.gz"
+    sha256 "90976a8a14ddf595f324ce852bf4933eca814dc3a4322c8f7aa4c08e70f357ef"
   end
 
   depends_on "go"
