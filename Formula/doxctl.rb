fb08721a5cabae1a99e51ec1e4639fb1ccaf78c3
goreleaserbot <goreleaser@lamolabs.org>
2021-05-18 23:13:04 -0400

    Brew formula update for doxctl version 0.0.3-alpha

--- Formula/doxctl.rb
+++ Formula/doxctl.rb
@@ -5,17 +5,17 @@
 class Doxctl < Formula
   desc "Software to create fast and easy drum rolls."
   homepage "https://www.github.com/slmingol/doxctl"
-  version "0.0.2-alpha"
+  version "0.0.3-alpha"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/slmingol/doxctl/releases/download/0.0.2-alpha/doxctl_0.0.2-alpha_Darwin_x86_64.tar.gz"
-    sha256 "2f6c2f5a59e7abf583fd9b9f7603200f29ff2e9ee2435d383ae4095d312ca0f9"
+    url "https://github.com/slmingol/doxctl/releases/download/0.0.3-alpha/doxctl_0.0.3-alpha_Darwin_x86_64.tar.gz"
+    sha256 "8d737441b4f9a727945284b7fc3d8fb9cbc60e49e873ba7cefb0a4649ef727c1"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/slmingol/doxctl/releases/download/0.0.2-alpha/doxctl_0.0.2-alpha_Linux_x86_64.tar.gz"
-    sha256 "f0009c209866a9932f9062b1a88af8f3a49d9e6cb6a1398b55cd541c50a498c6"
+    url "https://github.com/slmingol/doxctl/releases/download/0.0.3-alpha/doxctl_0.0.3-alpha_Linux_x86_64.tar.gz"
+    sha256 "729c19ee8dc499bc870364267c8a268de8a9c4254193381f95cd5ed98b75997e"
   end
 
   depends_on "go"
